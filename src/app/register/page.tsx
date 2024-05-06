4f40ba9b1a87eae23dc5913c8ae765cc07ecd048
unknown <smdemon3@gmail.com>
2024-05-06 15:19:59 +0600

    create login and registration page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -8,10 +8,10 @@ import {
   Typography,
 } from "@mui/material";
 import Image from "next/image";
-import Link from "next/link";
 import assets from "@/assets";
+import Link from "next/link";
 
-const LoginPage = () => {
+const Register = () => {
   return (
     <Container>
       <Stack
@@ -42,7 +42,7 @@ const LoginPage = () => {
             </Box>
             <Box>
               <Typography variant="h6" fontWeight={600}>
-                Login PH HealthCare
+                Patient Register
               </Typography>
             </Box>
           </Stack>
@@ -50,28 +50,45 @@ const LoginPage = () => {
           <Box>
             <form>
               <Grid container spacing={2} my={1}>
+                <Grid item md={12}>
+                  <TextField
+                    label="Name"
+                    fullWidth={true}
+                    name="patient.name"
+                  />
+                </Grid>
                 <Grid item md={6}>
                   <TextField
-                    name="email"
                     label="Email"
                     type="email"
                     fullWidth={true}
+                    name="patient.email"
                   />
                 </Grid>
                 <Grid item md={6}>
                   <TextField
-                    name="password"
                     label="Password"
                     type="password"
                     fullWidth={true}
+                    name="password"
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    label="Contact Number"
+                    type="tel"
+                    fullWidth={true}
+                    name="patient.contactNumber"
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    label="Address"
+                    fullWidth={true}
+                    name="patient.address"
                   />
                 </Grid>
               </Grid>
-
-              <Typography mb={1} textAlign="end" component="p" fontWeight={300}>
-                Forgot Password?
-              </Typography>
-
               <Button
                 sx={{
                   margin: "10px 0px",
@@ -79,13 +96,10 @@ const LoginPage = () => {
                 fullWidth={true}
                 type="submit"
               >
-                Login
+                Register
               </Button>
               <Typography component="p" fontWeight={300}>
-                Don&apos;t have an account?{" "}
-                <Link className="text-blue-600" href="/register">
-                  Create an account
-                </Link>
+                Do you already have an account? <Link href="/login" className="text-blue-600">Login</Link>
               </Typography>
             </form>
           </Box>
@@ -95,4 +109,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default Register;
