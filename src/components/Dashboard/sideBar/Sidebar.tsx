72aa41d0ca80b0d8e0d733f33f6ae08b17bbda6f
unknown <smdemon3@gmail.com>
2024-05-16 15:22:58 +0600

    create sidebar component

---
+++ src/components/Dashboard/sideBar/Sidebar.tsx
@@ -0,0 +1,75 @@
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MailIcon from "@mui/icons-material/Mail";
+import Divider from "@mui/material/Divider";
+import Toolbar from "@mui/material/Toolbar";
+import { Box, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+import assets from "@/assets";
+
+const Sidebar = () => {
+  const drawer = (
+    <div>
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+  return (
+    <Box>
+      <Stack
+        sx={{
+          py: 1,
+          mt: 1,
+        }}
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        gap={1}
+        component={Link}
+        href="/"
+      >
+        <Image src={assets.svgs.logo} width={40} height={40} alt="logo" />
+        <Typography
+          variant="h6"
+          component="h1"
+          sx={{
+            cursor: "pointer",
+          }}
+        >
+          SK Health Care
+        </Typography>
+      </Stack>
+      {drawer}
+    </Box>
+  );
+};
+
+export default Sidebar;
