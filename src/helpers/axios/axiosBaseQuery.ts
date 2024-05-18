ae3f7303fea9e86c2b9afe23475d2db9c922c5ae
unknown <smdemon3@gmail.com>
2024-05-18 21:06:45 +0600

    create axios instance

--- src/helpers/axios/axiosBaseQuery.ts
+++ src/helpers/axios/axiosBaseQuery.ts
@@ -1,7 +1,7 @@
 import { TMeta } from "@/types/common";
 import type { BaseQueryFn } from "@reduxjs/toolkit/query";
-import axios from "axios";
 import type { AxiosRequestConfig, AxiosError } from "axios";
+import { instance as axiosInstance } from "./axiosInstance";
 
 export const axiosBaseQuery =
   (
@@ -13,19 +13,22 @@ export const axiosBaseQuery =
       data?: AxiosRequestConfig["data"];
       params?: AxiosRequestConfig["params"];
       headers?: AxiosRequestConfig["headers"];
-      meta?: TMeta
+      meta?: TMeta,
+      contentType?: string
     },
     unknown,
     unknown
   > =>
-  async ({ url, method, data, params, headers }) => {
+  async ({ url, method, data, params, headers, contentType}) => {
     try {
-      const result = await axios({
+      const result = await axiosInstance({
         url: baseUrl + url,
         method,
         data,
         params,
-        headers,
+        headers: {
+          "Content-Type": contentType || "application/json"
+        }
       });
       return result;
     } catch (axiosError) {
---
+++ src/helpers/axios/axiosInstance.ts
@@ -0,0 +1,55 @@
+import { authKey } from "@/constact/authkey";
+import { ResponseSuccessType, TGenericErrorResponse } from "@/types/common";
+import { getFromLocalStorage } from "@/utils/local-storage";
+import axios from "axios";
+
+const instance = axios.create();
+instance.defaults.headers.post["Content-Type"] = "application/json";
+instance.defaults.headers["Accept"] = "application/json";
+instance.defaults.timeout = 60000;
+
+// Add a request interceptor
+instance.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+
+    const accessToken = getFromLocalStorage(authKey);
+    if (accessToken) {
+      config.headers.Authorization = accessToken;
+    }
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+instance.interceptors.response.use(
+  //@ts-ignore
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    const responseObject: ResponseSuccessType = {
+      data: response.data.data,
+      meta: response.data.meta,
+    };
+    return responseObject;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    // Do something with response error
+    const responseObject: TGenericErrorResponse = {
+      statusCode: error?.response?.data?.statusCode || 500,
+      message: error?.response?.data?.message || "Something went wrong!!!",
+      errorMessages: error?.response?.data?.message,
+    };
+    // return Promise.reject(error);
+    return responseObject;
+    return Promise.reject(error);
+  }
+);
+
+export { instance };
