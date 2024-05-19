63f1a8f5501d1e017ad011b69034963316e78458
unknown <smdemon3@gmail.com>
2024-05-19 14:59:45 +0600

    create reusable fullscreen modal, selected filed and doctor create modal

---
+++ src/app/(withDashboardLayout)/dashboard/admin/doctors/components/DoctorModal.tsx
@@ -0,0 +1,169 @@
+import { Button, Grid } from "@mui/material";
+import { FieldValues } from "react-hook-form";
+import { modifyPayload } from "@/utils/modifyPayload";
+import { toast } from "sonner";
+import SKFullscreenModal from "@/components/shared/SKFullscreenModal";
+import SKForm from "@/components/form/SKForm";
+import SKInput from "@/components/form/SKInput";
+import SKSelectField from "@/components/form/SKSelectedFields";
+import { Gender } from "@/types/common";
+
+type TProps = {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const DoctorModal = ({ open, setOpen }: TProps) => {
+  //   const [createDoctor] = useCreateDoctorMutation();
+
+  const handleFormSubmit = async (values: FieldValues) => {
+    // // console.log(values);
+    // values.doctor.experience = Number(values.doctor.experience);
+    // values.doctor.apointmentFee = Number(values.doctor.apointmentFee);
+    const data = modifyPayload(values);
+    try {
+      //   const res = await createDoctor(data).unwrap();
+      //   console.log(res);
+      //   if (res?.id) {
+      //     toast.success("Doctor created successfully!!!");
+      //     setOpen(false);
+      //   }
+    } catch (err: any) {
+      console.error(err);
+    }
+  };
+
+  const defaultValues = {
+    doctor: {
+      email: "",
+      name: "",
+      contactNumber: "",
+      address: "",
+      registrationNumber: "",
+      gender: "",
+      experience: 0,
+      apointmentFee: 0,
+      qualification: "",
+      currentWorkingPlace: "",
+      designation: "",
+      profilePhoto: "",
+    },
+    password: "",
+  };
+
+  return (
+    <SKFullscreenModal open={open} setOpen={setOpen} title="Create New Doctor">
+      <SKForm onSubmit={handleFormSubmit} defaultValues={defaultValues}>
+        <Grid container spacing={2} sx={{ my: 5 }}>
+          <Grid item xs={12} sm={12} md={4}>
+            <SKInput
+              name="doctor.name"
+              label="Name"
+              fullWidth={true}
+              sx={{ mb: 2 }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={4}>
+            <SKInput
+              name="doctor.email"
+              type="email"
+              label="Email"
+              fullWidth={true}
+              sx={{ mb: 2 }}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={12} md={4}>
+            <SKInput
+              name="password"
+              type="password"
+              label="Password"
+              fullWidth={true}
+              sx={{ mb: 2 }}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={12} md={4}>
+            <SKInput
+              name="doctor.contactNumber"
+              label="Contract Number"
+              fullWidth={true}
+              sx={{ mb: 2 }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={4}>
+            <SKInput
+              name="doctor.address"
+              label="Address"
+              fullWidth={true}
+              sx={{ mb: 2 }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={4}>
+            <SKInput
+              name="doctor.registrationNumber"
+              label="Registration Number"
+              fullWidth={true}
+              sx={{ mb: 2 }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={4}>
+            <SKInput
+              name="doctor.experience"
+              type="number"
+              label="Experience"
+              fullWidth={true}
+              sx={{ mb: 2 }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={4}>
+            <SKSelectField
+              items={Gender}
+              name="doctor.gender"
+              label="Gender"
+              sx={{ mb: 2 }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={4}>
+            <SKInput
+              name="doctor.apointmentFee"
+              type="number"
+              label="ApointmentFee"
+              fullWidth={true}
+              sx={{ mb: 2 }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={4}>
+            <SKInput
+              name="doctor.qualification"
+              label="Qualification"
+              fullWidth={true}
+              sx={{ mb: 2 }}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={12} md={4}>
+            <SKInput
+              name="doctor.currentWorkingPlace"
+              label="Current Working Place"
+              fullWidth={true}
+              sx={{ mb: 2 }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={4}>
+            <SKInput
+              name="doctor.designation"
+              label="Designation"
+              fullWidth={true}
+              sx={{ mb: 2 }}
+            />
+          </Grid>
+        </Grid>
+
+        <Button type="submit">Create</Button>
+      </SKForm>
+    </SKFullscreenModal>
+  );
+};
+
+export default DoctorModal;
